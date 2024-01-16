7b14db5890347ac6d2a04136b488a5d117e61052
Tushar Banik <b.toshar@iitg.ac.in>
2024-01-17 01:16:10 +0530

    button scrolldown

--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -1,4 +1,13 @@
+'use client'
+
 import { HiOutlineArrowNarrowDown } from "react-icons/hi";
+
+const Scroll = () => {
+    window.scrollTo({
+        top: window.innerHeight,
+        behavior: 'smooth'
+      });
+};
 
 const Hero = () => {
     return (
@@ -6,7 +15,7 @@ const Hero = () => {
             <strong className="text-8xl font-semibold">Tushar Banik</strong>
             <p className="text-6xl opacity-80 font-medium px-4">I build things for the web.</p>
             <p className="text-xl font-semibold hover:underline pl-4">Read more about me &rarr;</p>
-            <div className="absolute bottom-8 left-1/2 cursor-pointer">
+            <div className="absolute bottom-8 left-1/2 cursor-pointer" onClick={Scroll}>
                 <HiOutlineArrowNarrowDown size={20} />
             </div>
         </div>
