04ee80dc27cb57a55ebdec5077869f7606bc5d02
Tushar Banik <b.toshar@iitg.ac.in>
2024-01-17 00:54:35 +0530

    hero section

--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -1,9 +1,16 @@
+import { HiOutlineArrowNarrowDown } from "react-icons/hi";
+
 const Hero = () => {
-    return ( 
-        <div className="bg-black">
-hero
+    return (
+        <div className="bg-black text-white flex flex-col px-40 py-40 gap-4 items-start pb-80">
+            <strong className="text-8xl font-semibold">Tushar Banik</strong>
+            <p className="text-6xl opacity-80 font-medium px-4">I build things for the web.</p>
+            <p className="text-xl font-semibold hover:underline pl-4">Read more about me &rarr;</p>
+            <div className="absolute bottom-8 left-1/2 cursor-pointer">
+                <HiOutlineArrowNarrowDown size={20} />
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Hero;
--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -5,10 +5,10 @@ import { FaMoon } from "react-icons/fa";
 const Navbar = () => {
     return (
         <div className="bg-black flex flex-row justify-between text-white px-48 py-12">
-            <div className="font-black text-3xl">
+            <div className="font-black text-3xl underline">
                 T
             </div>
-            <div className="links flex flex-row gap-4 font-light">
+            <div className="links flex flex-row gap-4 font-semibold">
                 <Link href="/">Blog</Link>
                 <Link href="/">Projects</Link>
                 <Link href="/">About</Link>
